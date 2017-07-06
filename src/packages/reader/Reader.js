1f212ea704b4f6ce4e79e1b8bb796f70568242ba
dustin <dustinws@protonmail.com>
2017-07-05 21:28:46 -0400

    Add "Reader" type and FL bindings

--- src/packages/reader/Reader.js
+++ src/packages/reader/Reader.js
@@ -1,6 +1,8 @@
 import __ from 'ramda/src/__';
 import curry from 'ramda/src/curry';
-import { tag } from '../adt';
+import always from 'ramda/src/always';
+import compose from 'ramda/src/compose';
+import { tag, symbol } from '../adt';
 
 /**
  * @class Reader
@@ -25,7 +27,7 @@ const Reader = tag('Reader', 'run');
  * @description
  * Create a new reader that will return the given value.
  *
- * @since v1.0.1-beta
+ * @since v1.0.2-beta
  * @memberof Reader
  * @function of
  * @static
@@ -47,7 +49,7 @@ Reader.of = value =>
  * Run multiple readers in sequence. The function given to `.chain` must return
  * a `Reader`.
  *
- * @since v1.0.1-beta
+ * @since v1.0.2-beta
  * @memberof Reader
  * @function chain
  * @static
@@ -74,7 +76,7 @@ Reader.chain = curry((transform, reader) =>
  * @description
  * Apply a function to the value held by a `Reader`. Returns a new `Reader`.
  *
- * @since v1.0.1-beta
+ * @since v1.0.2-beta
  * @memberof Reader
  * @function map
  * @static
@@ -99,7 +101,7 @@ Reader.map = curry((transform, reader) =>
  * @description
  * Apply a function to the value held by a `Reader`. Returns a new `Reader`.
  *
- * @since v1.0.1-beta
+ * @since v1.0.2-beta
  * @memberof Reader
  * @function ap
  * @static
@@ -133,7 +135,7 @@ Reader.ap = curry((apply, reader) =>
  * Run multiple readers in sequence. The function given to `.chain` must return
  * a `Reader`.
  *
- * @since v1.0.1-beta
+ * @since v1.0.2-beta
  * @memberof Reader
  * @method
  * @instance
@@ -160,7 +162,7 @@ Reader.prototype.chain = function chain(transform) {
  * @description
  * Apply a function to the value held by a `Reader`. Returns a new `Reader`.
  *
- * @since v1.0.1-beta
+ * @since v1.0.2-beta
  * @memberof Reader
  * @method
  * @instance
@@ -185,7 +187,7 @@ Reader.prototype.map = function map(transform) {
  * @description
  * Apply a function to the value held by a `Reader`. Returns a new `Reader`.
  *
- * @since v1.0.1-beta
+ * @since v1.0.2-beta
  * @memberof Reader
  * @method
  * @instance
@@ -205,6 +207,202 @@ Reader.prototype.map = function map(transform) {
  */
 Reader.prototype.ap = function ap(apply) {
   return Reader.ap(apply, this);
+};
+
+
+/*
+ |------------------------------------------------------------------------------
+ | Monad Transformer
+ |------------------------------------------------------------------------------
+ */
+
+Reader.T = (M) => {
+  /**
+   * @class ReaderT
+   * @description
+   * The monad transformer for the `Reader` type. This is useful for 'stacking'
+   * monads together to get more functionality.
+   */
+  const ReaderT = tag(`Reader[${M[symbol]}]`, 'run');
+
+  /**
+   * @description
+   * `ReaderT.ask` returns a static `ReaderT` instance that you can pass
+   * operations to.
+   *
+   * @since v1.0.2-beta
+   * @memberof ReaderT
+   * @constant ask
+   * @static
+   * @type {ReaderT<E, Monad<A>>}
+   * @example
+   * // ask :: Monad m => ReaderT e m a
+   * import R from 'ramda';
+   * import { Reader, Task } from 'zoomjs';
+   *
+   * const RTask = Reader.T(Task);
+   *
+   * RTask.ask.map(R.toUpper).run('foo'); // Task(null, foo)
+   */
+  ReaderT.ask = ReaderT(M.of);
+
+  /**
+   * @description
+   * Lift an instance of the inner monad into a `ReaderT` and return it.
+   *
+   * @since v1.0.2-beta
+   * @memberof ReaderT
+   * @function lift
+   * @static
+   * @example
+   * // lift :: Monad m => m a -> ReaderT e m a
+   * import { Reader, Task } from 'zoomjs';
+   * import fs from 'fs';
+   *
+   * const RTask = Reader.T(Task);
+   *
+   * const readFile = Task.liftNode(fs.readFile);
+   *
+   * ReaderT.ask
+   *   .andThen(filepath =>
+   *     RTask.lift(readFile(filepath)))
+   *   .run('index.js');
+   *
+   * // => Task(Error, Buffer)
+   *
+   * @param  {Monad<A>} monad The monad to lift into the ReaderT. It must be
+   *     the same type of monad that was used to create the ReaderT.
+   * @return {ReaderT<E, Monad<A>>}
+   */
+  ReaderT.lift = compose(ReaderT, always);
+
+  /**
+   * @description
+   * Lift a value into a `ReaderT`
+   *
+   * @since v1.0.2-beta
+   * @memberof ReaderT
+   * @function of
+   * @static
+   * @implements Applicative
+   * @example
+   * // of :: Monad m => a -> ReaderT e m a
+   * import { Reader, Task } from 'zoomjs';
+   *
+   * const RTask = Reader.T(Task);
+   *
+   * RTask.of('foo'); // ReaderT(Env, Task(null, foo))
+   *
+   * @param  {A} value The value to put in the ReaderT
+   * @return {ReaderT<E, Monad<A>>}
+   */
+  ReaderT.of = value =>
+    ReaderT(() => M.of(value));
+
+  /**
+   * @description
+   * Run another ReaderT after the current one is finished. An alias for {@link ReaderT.andThen}
+   *
+   * @since v1.0.2-beta
+   * @memberof ReaderT
+   * @see {@link ReaderT.andThen}
+   * @method
+   * @instance
+   * @implements Chain
+   * @example
+   * // chain ReaderT e m a :: Monad m => (a -> ReaderT e m b) -> ReaderT e m b
+   * import { Reader, Task } from 'zoomjs';
+   *
+   * const RTask = Reader.T(Task);
+   *
+   * RTask.ask.chain(n => ReaderT.of(n + n)).run(10);
+   * // => ReaderT(10, Task(null, 20))
+   *
+   *
+   * @param  {function} transform The function to run on the inner value.
+   * @return {ReaderT<E, Monad<B>>}
+   */
+  ReaderT.prototype.chain = function chain(transform) {
+    return ReaderT(e => this.run(e).chain(a => transform(a).run(e)));
+  };
+
+  /**
+   * @description
+   * Run another ReaderT after the current one is finished. An alias for {@link ReaderT.chain}
+   *
+   * @since v1.0.2-beta
+   * @memberof ReaderT
+   * @see {@link ReaderT.chain}
+   * @method
+   * @instance
+   * @example
+   * // andThen ReaderT e m a :: Monad m => (a -> ReaderT e m b) -> ReaderT e m b
+   * import { Reader, Task } from 'zoomjs';
+   *
+   * const RTask = Reader.T(Task);
+   *
+   * RTask.ask.andThen(n => ReaderT.of(n + n)).run(10);
+   * // => ReaderT(10, Task(null, 20))
+   *
+   *
+   * @param  {function} transform The function to run on the inner value.
+   * @return {ReaderT<E, Monad<B>>}
+   */
+  ReaderT.prototype.andThen = function andThen(transform) {
+    return this.chain(transform);
+  };
+
+  /**
+   * @description
+   * Run a plain function on a value store in a `ReaderT`
+   *
+   * @since v1.0.2-beta
+   * @memberof ReaderT
+   * @method
+   * @instance
+   * @example
+   * // map ReaderT e m a :: Monad m => (a -> b) -> ReaderT e m b
+   * import { Reader, Task } from 'zoomjs';
+   *
+   * const RTask = Reader.T(Task);
+   *
+   * RTask.ask.map(n => n + n).run(10);
+   * // => ReaderT(10, Task(null, 20))
+   *
+   *
+   * @param  {function} transform The function to run on the inner value.
+   * @return {ReaderT<E, Monad<B>>}
+   */
+  ReaderT.prototype.map = function map(transform) {
+    return this.chain(x => ReaderT.of(transform(x)));
+  };
+
+  /**
+   * @description
+   * Run a function contained in an `Apply` on a value contained in a `ReaderT`
+   *
+   * @since v1.0.2-beta
+   * @memberof ReaderT
+   * @method
+   * @instance
+   * @example
+   * // ap ReaderT e m a :: Monad m => Apply (a -> b) -> ReaderT e m b
+   * import { Reader, Task } from 'zoomjs';
+   *
+   * const RTask = Reader.T(Task);
+   *
+   * RTask.ask.ap(ReaderT.of(n => n + n)).run(10);
+   * // => ReaderT(10, Task(null, 20))
+   *
+   *
+   * @param  {Apply<function>} apply An apply containing a function
+   * @return {ReaderT<E, Monad<B>>}
+   */
+  ReaderT.prototype.ap = function ap(apply) {
+    return ReaderT(e => this.run(e).ap(apply.run(e)));
+  };
+
+  return ReaderT;
 };
 
 export default Reader;
--- src/packages/reader/index.js
+++ src/packages/reader/index.js
@@ -1,3 +1,19 @@
+import FL from 'fantasy-land';
 import Reader from './Reader';
+
+// Reader Applicative
+Reader[FL.of] = Reader.of;
+
+// Reader Chain
+Reader[FL.chain] = Reader.chain;
+Reader.prototype[FL.chain] = Reader.prototype.chain;
+
+// Reader Functor
+Reader[FL.map] = Reader.map;
+Reader.prototype[FL.map] = Reader.prototype.map;
+
+// Reader Apply
+Reader[FL.ap] = Reader.ap;
+Reader.prototype[FL.ap] = Reader.prototype.ap;
 
 export default Reader;
